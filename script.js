f1807092672a3eb4d074aaf2a82127d748c344dd
Gulyapas Poonkawinsiri <joetlobb@gmail.com>
2025-10-19 18:49:17 -0700

    add first echart to the page

--- script.js
+++ script.js
@@ -18,3 +18,31 @@ button1.addEventListener("click", function() {
         alert("Total Players: " + data[0]["Total Players"]);
     })
 });
+
+// Initialize the echarts instance based on the prepared dom
+      var myChart = echarts.init(document.getElementById('viz'));
+
+      // Specify the configuration items and data for the chart
+      var option = {
+        title: {
+          text: 'ECharts Getting Started Example'
+        },
+        tooltip: {},
+        legend: {
+          data: ['sales']
+        },
+        xAxis: {
+          data: ['Shirts', 'Cardigans', 'Chiffons', 'Pants', 'Heels', 'Socks']
+        },
+        yAxis: {},
+        series: [
+          {
+            name: 'sales',
+            type: 'bar',
+            data: [5, 20, 36, 10, 10, 20]
+          }
+        ]
+      };
+
+      // Display the chart using the configuration items and data just specified.
+      myChart.setOption(option);
